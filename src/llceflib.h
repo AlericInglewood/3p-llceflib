2505352257779c0c401694bee10dc0b112a12033
callum_linden <devnull@localhost>
2015-09-21 16:55:05 -0700

    Silly omission of closing brace in LLCEFLib main version string - no logic changes

--- src/llceflib.h
+++ src/llceflib.h
@@ -36,7 +36,7 @@ class LLCEFLibImpl;
 
 // version information
 const std::string LLCEFLIB_BASE_VERSION = "1.1.0";
-const std::string CEF_VERSION_WIN = "(CEF-WIN-3.2454.1323-32";
+const std::string CEF_VERSION_WIN = "(CEF-WIN-3.2454.1323-32)";
 const std::string CEF_VERSION_OSX = "(CEF-OSX-3.2171.2069-32)";
 #ifdef _MSC_VER
 const std::string LLCEFLIB_VERSION = LLCEFLIB_BASE_VERSION + "-" + CEF_VERSION_WIN;
